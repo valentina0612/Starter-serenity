05ab308afade4ce6f1bd101956f37e50c6907531
valentina0612 <vbecag@correo.usbcali.edu.co>
2024-06-06 19:45:00 -0500

    First Web Test Serenity

---
+++ src/test/java/starter/duckduckgo/SearchResultSidebar.java
@@ -0,0 +1,8 @@
+package starter.duckduckgo;
+import net.serenitybdd.core.pages.PageComponent;
+
+public class SearchResultSidebar extends PageComponent {
+    public String heading() {
+        return $("[data-testid=about] h2").getText();
+    }
+}
